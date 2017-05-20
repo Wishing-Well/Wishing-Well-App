d4d03e973bf9b516c428e151c38d40370eb2f1a9
GaganTut <gstut@live.com>
2017-05-19 19:39:44 -1000

    Going to restart from scratch due to junk files

--- configureStore.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers';
-import ReduxThunk from 'redux-thunk';
-
-export default function configureStore() {
-  let store = createStore(reducers, applyMiddleware(ReduxThunk));
-  return store;
-}

de7aaacffe9019d70c784bbdd219ac5a0c65e6d7
GaganTut <gstut@live.com>
2017-05-21 18:06:59 -1000

    Sign up and login pages have minor validation to check for valid email inputs

--- WishingWell/src/containers/InitialPage/index.js
+++ WishingWell/src/containers/InitialPage/index.js
@@ -13,23 +13,48 @@ class InitialPage extends Component {
     super(props);
 
     this.state = {
-      username: '',
+      email: '',
       password: ''
     };
   }
 
-  handleLogin(){
-    this.props.login(this.state.username, this.state.password);
-  }
+  validateEmail = (email) => {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+  };
+
+  handleLogin = () => {
+    if(!this.validateEmail(this.state.email)) {
+      this.setState({email: '', password: ''});
+    } else {
+      this.props.login(this.state.username, this.state.password);
+    }
+  };
 
   render() {
     return (
       <View>
-        <TextInput style={styles.input} onChangeText={(username) => this.setState({username})}/>
-        <TextInput />
-        <Button title="Log In" onPress={this.handleLogin}/>
-        <Button title="Sign Up" onPress={() => this.props.navigate('Signup')}/>
-        <Button title="Map" onPress={() => this.props.navigate('MapPage')}/>
+        <TextInput
+          onChangeText={(email) => this.setState({email})}
+          value={this.state.email}
+          placeholder="Email"
+          />
+        <TextInput
+          onChangeText={(password) => this.setState({password})}
+          value={this.state.password}
+          placeholder="Password"
+          />
+        <Button
+          title="Log In"
+          onPress={this.handleLogin}
+          />
+        <Button
+          title="Sign Up"
+          onPress={() => this.props.navigate('Signup')}
+          />
+        <Button
+          title="Map"
+          onPress={() => this.props.navigate('MapPage')}
+          />
       </View>
     )
   }
--- WishingWell/src/containers/SignupPage/index.js
+++ WishingWell/src/containers/SignupPage/index.js
@@ -1,14 +1,55 @@
 import React, {Component} from 'react';
 import {
   View,
-  TextInput
+  TextInput,
+  Button
 } from 'react-native';
 
 class SignupPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullName: '',
+      email: '',
+      password: '',
+      togglePW: true
+    };
+  }
+
+  validateEmail = (email) => {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+  };
+
+  handleSignup = () => {
+    if(!this.validateEmail(this.state.email)) {
+      this.setState({email: ''});
+    }
+  };
+
   render() {
     return(
       <View>
-        <TextInput />
+        <TextInput
+          onChangeText={(fullName)=> this.setState({fullName})}
+          value={this.state.fullname}
+          placeholder="Enter Full Name"
+          />
+        <TextInput
+          onChangeText={(email)=> this.setState({email})}
+          value={this.state.email}
+          placeholder="Enter Email"
+          keyboardType="email-address"
+          />
+        <TextInput
+          onChangeText={(password)=> this.setState({password})}
+          secureTextEntry={this.state.togglePW}
+          value={this.state.password}
+          placeholder="Create Password"
+          />
+        <Button
+          title="Sign Up"
+          onPress={this.handleSignup}
+          />
       </View>
     )
   }

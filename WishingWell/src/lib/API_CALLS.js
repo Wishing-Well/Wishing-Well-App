3735fdd3ca49f909cf5b4feef0343054f9d895fe
GaganTut <gstut@live.com>
2017-05-22 10:39:31 -1000

    Created sign up fetch call to be used with db

--- WishingWell/src/lib/API_CALLS.js
+++ WishingWell/src/lib/API_CALLS.js
@@ -1,12 +1,23 @@
 /*jshint esversion: 6*/
 
-export const login = (username, password) => {
-  fetch('/api/users/login', {
+export const login = (username, password) => fetch('/api/users/login',
+  {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({username, password})
-  });
-};
+  }
+);
+
+export const signup = userInfo => fetch('/api/users/signup',
+  {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(userInfo)
+  }
+);

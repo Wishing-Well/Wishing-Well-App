b74c0ce3380cb967a89816b56b3ca1807eb23861
ctsygiel <ctsygiel@gmail.com>
2017-05-24 20:45:57 -1000

    working prototype of coin toss game

--- WishingWell/src/containers/StorePage/index.js
+++ WishingWell/src/containers/StorePage/index.js
@@ -8,6 +8,9 @@ import {
 } from 'react-native';
 import { connect } from 'react-redux';
 import styles from './stylesheet';
+import clamp from 'clamp';
+
+let SWIPE_THRESHOLD = 120;
 
 class StorePage extends Component {
 
@@ -18,12 +21,43 @@ class StorePage extends Component {
         };
 
         this.panResponder = PanResponder.create({
+          onMoveShouldSetResponderCapture: () => true,
+          onMoveShouldSetPanResponderCapture: () => true,
+
           onStartShouldSetPanResponder: () => true,
+
+          onPanResponderGrant: (e, gestureState) => {
+            this.state.pan.setOffset({x: this.state.pan.x._value, y: this.state.pan.y._value});
+            this.state.pan.setValue({x: 0, y: 0});
+          },
+
           onPanResponderMove: Animated.event([null, {
             dx: this.state.pan.x,
             dy: this.state.pan.y
           }]),
-          onPanResponderRelease: (e, gesture) => {}
+
+          onPanResponderRelease: (e, {vx, vy}) => {
+            this.state.pan.flattenOffset();
+            let velocity;
+
+            if (vy >= 0){
+              velocity = clamp(vx, 3, 5);
+            } else if (vy < 0){
+              velocity = clamp(vy * -1,3 ,5) * -1;
+            }
+
+            if (Math.abs(this.state.pan.y._value) > SWIPE_THRESHOLD) {
+              Animated.decay(this.state.pan, {
+                velocity: {x: vx, y: velocity},
+                deceleration: 0.98
+              }).start(this._resetState)
+            } else {
+              Animated.spring(this.state.pan, {
+                toValue: {x: 0, y: 0},
+                friction: 4
+              }).start()
+            }
+          }
         })
       };
 

4684151e292595000cd3f7f1d10033fb2c9d5f56
GaganTut <gstut@live.com>
2017-05-30 18:39:31 -1000

    Styling the store page a lil bit

--- WishingWell/src/containers/StorePage/styles.js
+++ WishingWell/src/containers/StorePage/styles.js
@@ -7,57 +7,43 @@ import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
   wholePage: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#2B2B2B',
-    justifyContent: 'center',
+
   },
   textContainer: {
-    flex: 1
+
   },
   title: {
-    flex: 2,
-    fontSize: 50,
-    color: '#84DBEF'
+
   },
   subtitle: {
-    flex: 1,
-    fontSize: 20,
-    color: '#84DBEF'
+
   },
   firstRow: {
-    flex: 2,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center'
+
   },
   secondRow: {
-    flex: 2
+
   },
   tenDollars: {
-    width: 125,
-    height: 125,
-    alignItems: 'center',
-    justifyContent: 'center'
+    padding: '5%'
   },
   fiftyDollars: {
-    flex: 1,
-    width: 125,
-    height: 125,
-    alignItems: 'center',
-    justifyContent: 'center'
+
   },
   tenPic: {
-    width: 125,
-    height: 125
+    width: 100,
+    height: 100,
+    margin: '5%'
   },
   fiftyPic: {
-    width: 125,
-    height: 125
+    width: 100,
+    height: 100,
+    margin: '5%'
   },
   hundredPic: {
     width: 180,
-    height: 180
+    height: 180,
+    margin: '5%'
   }
 
 });
